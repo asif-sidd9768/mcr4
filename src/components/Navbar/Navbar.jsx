b60c0bdb911951fd34c47717c552d32cd80d6efd
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-06-24 21:41:41 +0530

    adde navbar  text

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import "./Navbar.css"
 
 export const Navbar = () => {
   return (
-    <div className="navbar-container">Navbar</div>
+    <div className="navbar-container">
+      <p className="navbar-text"><span className="whimsy-text">Whimsy</span><span className="zone-text">Zone</span></p>
+    </div>
   )
 }
